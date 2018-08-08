1528841110906d14be4b09554f36523aebeb3876
Jia Rong <jr.loves.maths@gmail.com>
2018-08-08 14:55:06 +0800

    feat: added comments on type files

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
+// This exposes internal types under the 'Switcheo' namespace for external usage.
+// See package.json:"types"
 export * from './lib/types'
 
 export as namespace Switcheo
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,5 @@
+// This is used internally for this project.
+// See src/index.d.ts
 export * from './blockchain'
 export * from './net'
 export * from './params'
