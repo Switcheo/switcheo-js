a96e38f1713bfcfab4a6073715e52c529e1d4f64
Ivan Poon <ravenxce@gmail.com>
2018-09-20 14:35:43 +0800

    Add deposit api

--- src/lib/api/balances/index.ts
+++ src/lib/api/balances/index.ts
@@ -1,6 +1,9 @@
 import { Account, Config } from '../../switcheo'
 import req from '../../req'
 import { wallet as neonWallet } from '@cityofzion/neon-core'
+import { Blockchain } from '../../constants'
+import { performMultistepRequest } from '../helpers'
+import BigNumber from 'bignumber.js'
 
 export function get(config: Config,
   accounts: Account | ReadonlyArray<Account>): Promise<object> {
@@ -16,4 +19,43 @@ export function getNeoAssets(config: Config, account: Account): Promise<object> 
   const url: string =
     `${config.url}/address/balance/${neonWallet.getAddressFromScriptHash(account.address)}`
   return req.get(url)
+}
+
+interface AssetLike {
+  blockchain: Blockchain
+  scriptHash: string
+  decimals: number
+}
+
+export function deposit(config: Config, account: Account,
+  asset: AssetLike, amount: BigNumber | string): Promise<any> {
+  const amountWithoutPrecision: string =
+    new BigNumber(amount).times(10 ** asset.decimals).toString()
+
+  if (asset.blockchain === Blockchain.Neo) {
+    return depositNeoAsset(config, account, asset.scriptHash, amountWithoutPrecision)
+  }
+
+  throw new Error('Not yet implemented for this blockchain!')
+}
+
+interface DepositParams {
+  readonly blockchain: Blockchain
+  readonly contractHash: string
+  readonly assetID: string
+  readonly amount: string
+}
+
+function depositNeoAsset(config: Config, account: Account,
+  assetID: string, amount: string): Promise<any> {
+  const contractHash: string = config.getContractHash(Blockchain.Neo)
+  const params: DepositParams = { blockchain: Blockchain.Neo, contractHash, assetID, amount }
+
+  return performMultistepRequest(
+    config,
+    '/deposits',
+    result => `/deposits/${result.id}/broadcast`,
+    params,
+    account
+  ) as Promise<any>
 }
