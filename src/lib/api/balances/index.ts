76d92e07663aee79519fa08355f54c6db197c1b2
Ivan Poon <ravenxce@gmail.com>
2018-09-22 15:39:02 +0800

    Add withdrawal api

--- src/lib/api/balances/index.ts
+++ src/lib/api/balances/index.ts
@@ -5,6 +5,19 @@ import { Blockchain } from '../../constants'
 import { performMultistepRequest } from '../helpers'
 import BigNumber from 'bignumber.js'
 
+interface AssetLike {
+  blockchain: Blockchain
+  scriptHash: string
+  decimals: number
+}
+
+interface TransferParams {
+  readonly blockchain: Blockchain
+  readonly contractHash: string
+  readonly assetId: string
+  readonly amount: string
+}
+
 export function get(config: Config,
   accounts: Account | ReadonlyArray<Account>): Promise<object> {
   const wrappedAccounts = Array.isArray(accounts) ? accounts : [accounts]
@@ -21,12 +34,6 @@ export function getNeoAssets(config: Config, account: Account): Promise<object> 
   return req.get(url)
 }
 
-interface AssetLike {
-  blockchain: Blockchain
-  scriptHash: string
-  decimals: number
-}
-
 export function deposit(config: Config, account: Account,
   asset: AssetLike, amount: BigNumber | string): Promise<any> {
   const amountWithoutPrecision: string =
@@ -39,22 +46,41 @@ export function deposit(config: Config, account: Account,
   throw new Error('Not yet implemented for this blockchain!')
 }
 
-interface DepositParams {
-  readonly blockchain: Blockchain
-  readonly contractHash: string
-  readonly assetId: string
-  readonly amount: string
+export function withdraw(config: Config, account: Account,
+  asset: AssetLike, amount: BigNumber | string): Promise<any> {
+  const amountWithoutPrecision: string =
+    new BigNumber(amount).times(10 ** asset.decimals).toString()
+
+  if (asset.blockchain === Blockchain.Neo) {
+    return withdrawNeoAsset(config, account, asset.scriptHash, amountWithoutPrecision)
+  }
+
+  throw new Error('Not yet implemented for this blockchain!')
 }
 
 function depositNeoAsset(config: Config, account: Account,
   assetId: string, amount: string): Promise<any> {
   const contractHash: string = config.getContractHash(Blockchain.Neo)
-  const params: DepositParams = { blockchain: Blockchain.Neo, contractHash, assetId, amount }
+  const params: TransferParams = { blockchain: Blockchain.Neo, contractHash, assetId, amount }
 
   return performMultistepRequest(
     config,
     '/deposits',
     result => `/deposits/${result.id}/broadcast`,
+    params,
+    account
+  ) as Promise<any>
+}
+
+function withdrawNeoAsset(config: Config, account: Account,
+  assetId: string, amount: string): Promise<any> {
+  const contractHash: string = config.getContractHash(Blockchain.Neo)
+  const params: TransferParams = { blockchain: Blockchain.Neo, contractHash, assetId, amount }
+
+  return performMultistepRequest(
+    config,
+    '/withdrawals',
+    result => `/withdrawals/${result.id}/broadcast`,
     params,
     account
   ) as Promise<any>
