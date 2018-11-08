c3d6d4b1fb5f25d3b1588fdfcc6122906b6bdf2e
John <jwwwwong@gmail.com>
2018-11-08 15:04:08 +0800

    Fix production eth contract hash

--- src/lib/constants/contracts.ts
+++ src/lib/constants/contracts.ts
@@ -35,7 +35,7 @@ export const CONTRACT_HASHES: ContractHashes = {
       V2: '0x607af5164d95bd293dbe2b994c7d8aef6bec03bf',
     },
     [Network.MainNet]: {
-      V2: '0xe777047Ee330200775D7fB7e0051432f68C36821',
+      V2: '0xe777047ee330200775d7fb7e0051432f68c36821',
     },
   },
   [Blockchain.Qtum]: {
