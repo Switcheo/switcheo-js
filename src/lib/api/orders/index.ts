07244f40014df0396b5afb791d8a5ac025a49955
Ivan Poon <ravenxce@gmail.com>
2018-11-19 13:27:27 +0800

    Use fixed package for ledger u2f
    See: https://github.com/LedgerHQ/ledgerjs/issues/218#issuecomment-439083967

--- src/lib/api/orders/index.ts
+++ src/lib/api/orders/index.ts
@@ -1,7 +1,14 @@
 import { broadcast } from './broadcast'
-import { create } from './create'
-import { get } from './get'
+import { create, CreateOrderParams } from './create'
+import { get, ListOrdersParams } from './get'
 import { make } from './make'
 
-// tslint:disable-next-line typedef
-export const orders = { broadcast, create, get, make }
+import { Order } from '../../models/order'
+import { Account, Config } from '../../switcheo'
+
+export const orders: {
+  broadcast: (config: Config, account: Account, order: Order) => Promise<Order>
+  create: (config: Config, account: Account, orderParams: CreateOrderParams) => Promise<Order>
+  get: (config: Config, account: Account, listOrdersParams: ListOrdersParams) => Promise<object>
+  make: (config: Config, account: Account, orderParams: CreateOrderParams) => Promise <Order>
+} = { broadcast, create, get, make }
