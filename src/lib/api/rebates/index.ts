da8b343b5cd95752509fcc9bb8ddea041f9d7341
Jun Wei Chan <sj@Juns-MacBook-Pro.local>
2018-12-26 10:34:32 +0800

    update function names

--- src/lib/api/rebates/index.ts
+++ src/lib/api/rebates/index.ts
@@ -7,7 +7,7 @@ export type RebatesGetResponse = {
   [key in AssetSymbol]: { paid: number, unpaid: number }
 }
 
-export function get(config: Config,
+export function getRebateBalance(config: Config,
   accounts: Account | ReadonlyArray<Account>): Promise<RebatesGetResponse> {
   const wrappedAccounts: ReadonlyArray<Account> = Array.isArray(accounts) ? accounts : [accounts]
   const addresses: ReadonlyArray<string> =
@@ -18,7 +18,7 @@ export function get(config: Config,
 
 export type RebatesHistoryResponse = ReadonlyArray<History>
 
-export function history(config: Config, accounts: Account | ReadonlyArray<Account>):
+export function getRebateHistory(config: Config, accounts: Account | ReadonlyArray<Account>):
 Promise<RebatesHistoryResponse> {
   const wrappedAccounts: ReadonlyArray<Account> = Array.isArray(accounts) ? accounts : [accounts]
   const addresses: ReadonlyArray<string> =
