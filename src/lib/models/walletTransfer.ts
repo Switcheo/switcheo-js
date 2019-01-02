f510eaac7f2717a08d98c873b52a12244e0970a0
Lim Jia Rong <jr.loves.maths@gmail.com>
2019-01-02 16:56:03 +0800

    Auto transfer (#6)
    - Withdraw creates a WalletTransfer
    - create WalletTransfer, get incompleteWithdrawals,
    transfer (create txn, broadcast txn) lib

---
+++ src/lib/models/walletTransfer.ts
@@ -0,0 +1,31 @@
+import { Blockchain } from '../constants'
+import { AssetSymbol } from './assets'
+
+enum TransferType {
+  Withdrawal = 'withdrawal',
+}
+
+export enum WalletTransferStatus {
+  Pending = 'pending',
+  Confirming = 'confirming',
+  Success = 'success',
+  Expired = 'expired',
+}
+
+export interface WalletTransfer {
+  fromAddress: string
+  toAddress: string
+  amount: number
+  assetId: string
+  assetSymbol: AssetSymbol
+  blockchain: Blockchain
+  contractHash: string
+  createdAt: string
+  transferType: TransferType
+  id: string
+  balanceId: string
+  status: WalletTransferStatus
+  isBalanceStatusActive: boolean
+  transactionHash: string
+  updatedAt: string
+}
